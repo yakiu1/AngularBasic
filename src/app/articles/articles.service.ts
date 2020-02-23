4214cdf44b7db5534e18564898ad9bbac7d1006d
yakiu1 <yakiu100@gmail.com>
2020-02-23 15:02:13 +0800

    Gon 寫Service練習

--- src/app/articles/articles.service.ts
+++ src/app/articles/articles.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ArticlesDataInterface } from './articlesDataInterface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticlesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getWords() {
+    // now returns an Observable of Config
+    return this.http.get<ArticlesDataInterface>('./api/apiData.json');
+  }
 }
