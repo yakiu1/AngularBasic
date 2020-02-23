a64104aa2dc9a6aef55a4748c57ff8317e716726
yakiu1 <yakiu100@gmail.com>
2020-02-23 10:35:30 +0800

    Gon 雙向繫結 練習
    沒使用ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'conduit';
   subtitle = 'A place to share Gon';
-  linkText = 'knowledge.';
   linkurl = 'https://pvt5r486.github.io/f2e/20190527/123375935/';
   linkHtml = '<u>knowledge.</u>';
 }
